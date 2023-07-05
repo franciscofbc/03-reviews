a3735e6ddb5563d2326197041bf8f410fbee894a
Francisco Berti da Cruz <fbc991@live.com>
2023-07-04 22:52:21 -0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,64 @@
 import { useEffect, useState } from 'react';
 import reviews from './data';
+import { FaQuoteRight } from 'react-icons/fa';
 
 const App = () => {
-  const [index, setIndex] = useState(1);
-  const [person, setPerson] = useState({});
+  const [index, setIndex] = useState(0);
+  const [review, setReview] = useState({});
 
-  const findPerson = () => {
-    setPerson(reviews.find((review) => review.id === index));
+  const findReview = (index) => {
+    setReview(reviews[index]);
+  };
+
+  const previous = () => {
+    if (index === 0) {
+      setIndex(reviews.length - 1);
+      return;
+    }
+    setIndex(index - 1);
+  };
+
+  const next = () => {
+    if (index < reviews.length - 1) {
+      setIndex(index + 1);
+      return;
+    }
+    setIndex(0);
+  };
+
+  const surprise = () => {
+    // Math.random() * (max - min) + min;
+    setIndex(Math.round(Math.random() * (reviews.length - 1 - 0) + 0));
   };
 
   useEffect(() => {
-    findPerson();
-  }, []);
+    findReview(index);
+  }, [index]);
 
   return (
     <main>
-      <section>
-        <h2>{person.name}</h2>
-        <p>{person.job}</p>,
-        <img src={person.image} alt={person.name} width={'50px'} />
-        <p>{person.text}</p>
-        <button>anterior</button>
-        <button>seguinte</button>
+      <section className="container">
+        <div className="img-complete">
+          <div className="img-back"></div>
+          <img src={review.image} alt={review.name} className="img avatar" />
+          <span className="quote">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="name">{review.name}</h4>
+        <p className="job">{review.job}</p>
+        <p className="text-review">{review.text}</p>
+        <div>
+          <button onClick={previous} className="btn btn-pre-next">
+            &lt;
+          </button>
+          <button onClick={next} className="btn btn-pre-next">
+            &gt;
+          </button>
+        </div>
+        <button onClick={surprise} className="btn btn-hipster">
+          surprise me
+        </button>
       </section>
     </main>
   );
